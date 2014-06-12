ce56fa091663c103dab03eba00ca2dd8231924c3
Wr4th <ale23pandado@gmail.com>
2014-06-12 10:58:32 +0200

    Aggiunte credenziali Mysql
    Aggiunte le credenziali per l'accesso mysql al file di configurazione

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 	//Credenziali per il database
 
-	define("DB_USERNAME", 'username');
-	define("DB_PASS", 'password');
-	define("DB_NAME", 'database');
+	define("DB_USERNAME", 'root');
+	define("DB_PASS", '');
+	define("DB_NAME", 'loccioniserver');
 
 ?>
